0ebdfdc3f70fc40709a3927b51c80e3f3f39fcf7
adkins-david <dadkins888@gmail.com>
2023-08-22 20:28:30 -0400

    More imports

--- virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPet.java
+++ virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPet.java
@@ -1,9 +1,8 @@
 package com.wcci.amazon.b4.virtualpet;
 
-import java.util.Date;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
