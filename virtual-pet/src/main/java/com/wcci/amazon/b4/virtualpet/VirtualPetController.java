b873df3f886bd83876503494be7803060ed3db92
adkins-david <dadkins888@gmail.com>
2023-08-25 18:34:38 -0400

    Got repository working

--- virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetController.java
+++ virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetController.java
@@ -2,6 +2,8 @@ package com.wcci.amazon.b4.virtualpet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.util.function.Supplier;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,10 +25,19 @@ import java.util.Optional;
         this.virtualPetRepository = virtualPetRepository;
       }
       @GetMapping("/{id}")
-      public VirtualPet getPetById(@PathVariable Long id) {
-        return virtualPetRepository.findById(id)
-            .orElseThrow(() -> new PetNotFoundException("id"));
-    }
+      /*public Supplier<Throwable> supplier() {*/
+      public ResponseEntity<VirtualPet> getPetById(@PathVariable Long id) {
+        Optional<VirtualPet>=virtualPetRepository.findById(id);
+
+        if (virtualPet.isPresent()) {
+          return new ResponseEntity<>(virtualPet.get(), HttpStatus.OK);
+        }else{
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+            /* .orElseThrow(() -> new PetNotFoundException("id"));*/
+      }
+    /* } */
       @PostMapping
       public VirtualPet addPet(@RequestBody VirtualPet newPet) {
         return virtualPetRepository.save(newPet);
--- virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetRepository.java
+++ virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetRepository.java
@@ -1,6 +1,8 @@
 package com.wcci.amazon.b4.virtualpet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+/*import org.springframework.stereotype.Repository;*/
 
-public interface VirtualPetRepository extends JpaRepository<VirtualPet, Long>
+public interface VirtualPetRepository extends JpaRepository<VirtualPet, Long>{
+
+}
