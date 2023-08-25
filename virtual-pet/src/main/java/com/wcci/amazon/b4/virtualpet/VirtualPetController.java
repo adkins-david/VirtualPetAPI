8421cb7ff70c26e4916cb4a881eca50b49c3c375
adkins-david <dadkins888@gmail.com>
2023-08-25 19:06:34 -0400

    Almost have controller

--- virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetController.java
+++ virtual-pet/src/main/java/com/wcci/amazon/b4/virtualpet/VirtualPetController.java
@@ -1,6 +1,76 @@
 package com.wcci.amazon.b4.virtualpet;
 
-import org.springframework.boot.SpringApplication;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VirtualPetController {
+    @Autowired
+    VirtualPetRepository virtualPetRepository;
+
+    @GetMapping("/api/pets/{id}")
+    public ResponseEntity<VirtualPet> getVirtualPet(@PathVariable("id") long id) {
+        Optional<VirtualPet> virtualPet = virtualPetRepository.findById(id);
+
+        if (virtualPet.isPresent()) {
+            return new ResponseEntity<>(virtualPet.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/api/pets/")
+    public ResponseEntity<List<VirtualPet>> getAllVirtualPet() {
+        List<VirtualPet> virtualPets = virtualPetRepository.findAll();
+
+        if (virtualPets.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<List<VirtualPet>>(virtualPets, HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/api/pets/add")
+    public ResponseEntity<VirtualPet> createVirtualPet(@RequestBody VirtualPet virtualPet) {
+        VirtualPet virtualPets = virtualPetRepository
+                .save(new VirtualPet());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/api/pets/{id}")
+      public void deletePet(@PathVariable Long id) {
+        virtualPetRepository.deleteById(id);
+      }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +96,7 @@ import java.util.Optional;
       }
       @GetMapping("/api/pets/{id}")
       /*public Supplier<Throwable> supplier() {*/
-      public ResponseEntity<VirtualPet> getPetById(@PathVariable Long id) {
+     /*  public ResponseEntity<VirtualPet> getPetById(@PathVariable Long id) {
         Optional<VirtualPet>=virtualPetRepository.findById(id);
 
         if (VirtualPet.isPresent()) {
@@ -36,16 +106,16 @@ import java.util.Optional;
         }
 
             /* .orElseThrow(() -> new PetNotFoundException("id"));*/
-      }
+      //}
     /* } */
-      @PostMapping("/api/pets/{id}")
-      public VirtualPet addPet(@RequestBody VirtualPet newPet) {
-        return virtualPetRepository.save(newPet);
-    }
-      @DeleteMapping("/api/pets/{id}")
-      public void deletePet(@PathVariable Long id) {
-        virtualPetRepository.deleteById(id);
-    }
-    }
+      //@PostMapping("/api/pets/{id}")
+      //public VirtualPet addPet(@RequestBody VirtualPet newPet) {
+        //return virtualPetRepository.save(newPet);
+    //}
+      //@DeleteMapping("/api/pets/{id}")
+      //public void deletePet(@PathVariable Long id) {
+       // virtualPetRepository.deleteById(id);
+    //}
+    //} 
 
 
